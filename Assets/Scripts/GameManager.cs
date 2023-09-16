56595813841f22617916afb4f5dcb2b891c4c0c1
Roger Wang <rogerwang2520@gmail.com>
2023-09-15 20:31:58 -0500

    Add GameManager, PlayerController

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    [SerializeField] float timeToWin;
+    [SerializeField] float decreaseRate = 1f;
+    float timer = 0f;
+
+    [SerializeField] PlayerController player;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject); 
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void UpdateTimer()
+    {
+        if (true)
+        {
+            timer += Time.deltaTime;
+        }
+        else if (true)
+        {
+            timer -= Time.deltaTime * decreaseRate;
+        }
+    }
+
+    public bool HasWon()
+    {
+        return timer >= timeToWin;
+    }
+}
