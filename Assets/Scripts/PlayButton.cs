d0ecfa49c40e79b6980903a847fb3424b211ec1a
Roger Wang <rogerwang2520@gmail.com>
2023-09-15 22:58:02 -0500

    Add button sound

--- Assets/Scripts/PlayButton.cs
+++ Assets/Scripts/PlayButton.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using System;
 
 public class PlayButton : MonoBehaviour
 {
