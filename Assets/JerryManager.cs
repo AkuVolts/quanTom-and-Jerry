ea45d3bc4cb6f1888f81ac3bbfc9618a132b79bb
Revan Ji <jiruifancr@gmail.com>
2023-09-15 23:53:43 -0500

    seemingly functional jerry controller

--- Assets/JerryManager.cs
+++ Assets/JerryManager.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Data.Common;
+using System.Linq;
 using UnityEngine;
 
 public class JerryManager : MonoBehaviour
 {
 	public float coolDownTime;
-	private bool _inToggleCooldown;
+	private bool _inToggleCooldown = false;
 	private int _cacheJerryCertaintyChange;
 
 	public Transform topLeftAnchor;
@@ -19,20 +20,18 @@ public class JerryManager : MonoBehaviour
 	private float _topYExtent;
 	private float _bottomYExtent;
 
-	private Vector2 _widthHeightVector;
-	
+	[SerializeField] private GameObject jerryPrefab;
 
 	[SerializeField] private GameObject trueJerry;
 
 	class Jerry
 	{
-		public Transform transform;
-		public Vector2 velocity = new Vector2(0, 1);
-		public float speed = 5;
-		public Vector2 pastPosition = new Vector2(0,0);
+		public Transform Transform;
+		public Vector2 Velocity = new Vector2(0, 1);
+		public float Speed = 5;
 	}
 	// 10 is max level of certainty where jerry is running amok
-	private int levelOfCertainty = 1;
+	private int _levelOfCertainty = 1;
 
 	// we ensure that _jerries[0] is true jerry
 	private List<Jerry> _jerries = new List<Jerry>();
@@ -42,31 +41,31 @@ public class JerryManager : MonoBehaviour
     void Start()
     {
 	    _jerries.Add(new Jerry());
-	    _jerries[0].transform = trueJerry.transform;
+	    _jerries[0].Transform = trueJerry.transform;
+
+	    var topLeftPos = topLeftAnchor.position;
+	    var topRightPos = bottomRightAnchor.position;
 	    
-	    _leftXExtent = topLeftAnchor.position.x;
-	    _rightXExtent = bottomRightAnchor.position.x;
-	    _topYExtent = topLeftAnchor.position.y;
-	    _bottomYExtent = bottomRightAnchor.position.y;
+	    _leftXExtent = topLeftPos.x;
+	    _rightXExtent = topRightPos.x;
+	    _topYExtent = topLeftPos.y;
+	    _bottomYExtent = topRightPos.y;
 	    
 	    _screenWidth = _rightXExtent - _leftXExtent;
 	    _screenHeight = _topYExtent - _bottomYExtent;
-	    _widthHeightVector = new Vector2(_screenWidth, _screenHeight);
     }
 
     private void UpdateJerryPositions()
     {
 	    foreach (var jerry in _jerries)
 	    {
-		    jerry.pastPosition = jerry.transform.position;
 		    float randomRotate = Random.Range(-20f, 20f);
 		    float randomSpeedChange = Random.Range(-0.2f, 0.2f);
-		    jerry.velocity = RotateVector(jerry.velocity, randomRotate).normalized;
-		    jerry.speed = Mathf.Clamp(jerry.speed + randomSpeedChange, 2f, 8f);
-		    var addSpeed = jerry.velocity * jerry.speed * Time.fixedDeltaTime;
-		    var newPos = new Vector2(
-			    jerry.transform.position.x,
-			    jerry.transform.position.y) + addSpeed;
+		    jerry.Velocity = RotateVector(jerry.Velocity, randomRotate).normalized;
+		    jerry.Speed = Mathf.Clamp(jerry.Speed + randomSpeedChange, 2f, 8f);
+		    var addSpeed = jerry.Velocity * jerry.Speed * Time.fixedDeltaTime;
+		    var position = jerry.Transform.position;
+		    var newPos = new Vector2(position.x, position.y) + addSpeed;
 		    
 		    Vector2 normalizedPos = newPos - new Vector2(_leftXExtent, _bottomYExtent);
 		    
@@ -75,31 +74,48 @@ public class JerryManager : MonoBehaviour
 		    
 		    newPos = normalizedPos + new Vector2(_leftXExtent, _bottomYExtent);
 		    
-		    jerry.transform.position = new Vector3(newPos.x, newPos.y, 0f);
+		    position = new Vector3(newPos.x, newPos.y, 0f);
+		    jerry.Transform.position = position;
 	    }
     }
     
     public Vector2 RotateVector(Vector2 v, float angle)
     {
 	    float radian = angle*Mathf.Deg2Rad;
-	    float _x = v.x*Mathf.Cos(radian) - v.y*Mathf.Sin(radian);
-	    float _y = v.x*Mathf.Sin(radian) + v.y*Mathf.Cos(radian);
-	    return new Vector2(_x,_y);
+	    float x = v.x*Mathf.Cos(radian) - v.y*Mathf.Sin(radian);
+	    float y = v.x*Mathf.Sin(radian) + v.y*Mathf.Cos(radian);
+	    return new Vector2(x,y);
     }
 
     private void DuplicateJerries()
     {
-	    
-    }
-
-    private void SpawnJerry(Vector2 targetPos)
-    {
-	    
+	    int jerryCount = _jerries.Count;
+	    for (int i = 0; i < jerryCount; i++)
+	    {
+		    var newJerryObject = Instantiate(jerryPrefab, gameObject.transform);
+		    newJerryObject.transform.position = _jerries[i].Transform.position;
+		    var newJerry = new Jerry
+		    {
+			    Speed = _jerries[i].Speed,
+			    Velocity = _jerries[i].Velocity,
+			    Transform = newJerryObject.transform
+		    };
+		    _jerries.Add(newJerry);
+	    }
     }
 
     private void DecimateJerries()
     {
-	    
+	    var rnd = new System.Random();
+	    var jerriesToDelete = _jerries
+		    .Skip(1)
+		    .OrderBy(x => rnd.Next())
+		    .Take(_jerries.Count / 2);
+	    foreach (var badJerry in jerriesToDelete)
+	    {
+		    Destroy(badJerry.Transform.gameObject);
+		    _jerries.Remove(badJerry);
+	    }
     }
 
     private IEnumerator CoolDownCoro()
@@ -115,37 +131,34 @@ public class JerryManager : MonoBehaviour
 	    if (!_inToggleCooldown)
 	    {
 		    _cacheJerryCertaintyChange = 0;
-		    if (Input.GetKeyDown(KeyCode.LeftArrow))
+		    if (Input.GetKeyDown(KeyCode.RightArrow))
 		    {
 			    _cacheJerryCertaintyChange += 1;
 		    }
-		    if (Input.GetKeyDown(KeyCode.RightArrow))
+		    if (Input.GetKeyDown(KeyCode.LeftArrow))
 		    {
 			    _cacheJerryCertaintyChange -= 1;
 		    }
 		    if (_cacheJerryCertaintyChange != 0)
 		    {
 			    StartCoroutine(CoolDownCoro());
-		    }
+		    } 
 	    }
     }
 
 	void FixedUpdate() {
-		/*
-		 * Every frame I want to
-		 * check if the level of certainty has changed
-		 * if true: generate/despawn jerry
-		 * move jerry according to level of certainty
-		 * 
-		 */
-		if (_cacheJerryCertaintyChange != 0)
+		switch (_cacheJerryCertaintyChange)
 		{
-			_cacheJerryCertaintyChange = 0;
-			
+			case 1:
+				DuplicateJerries();
+				_cacheJerryCertaintyChange = 0;
+				break;
+			case -1:
+				DecimateJerries();
+				_cacheJerryCertaintyChange = 0;
+				break;
 		}
-
 		UpdateJerryPositions();
-
 	}
 	
 }
